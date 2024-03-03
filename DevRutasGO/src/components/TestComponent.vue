b656f1237c55e719eddbcc122b493715ae65985c
NoCap940 <riplui666@gmail.com>
2024-03-03 00:59:05 -0600

    Elimine el saludo de la pagina

--- DevRutasGO/src/components/TestComponent.vue
+++ DevRutasGO/src/components/TestComponent.vue
@@ -9,7 +9,7 @@ export default {
     name: 'TestComponent',
     data() {
         return {
-            message: 'Aqui vamos a comenzar el proyecto "RutasGO, hola lui"'
+            message: 'Aqui vamos a comenzar el proyecto "RutasGO"'
         }
     }
 }
