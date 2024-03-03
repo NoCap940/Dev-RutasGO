bfdd5cf2b18f01edb05c651278cf6464bf87d817
NoCap940 <riplui666@gmail.com>
2024-03-02 19:07:54 -0600

    Creacion del primer componente de prueba

---
+++ DevRutasGO/src/components/TestComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="test-component">
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TestComponent',
+    data() {
+        return {
+            message: 'Aqui vamos a comenzar el proyecto "RutasGO"'
+        }
+    }
+}
+
+
+</script>
