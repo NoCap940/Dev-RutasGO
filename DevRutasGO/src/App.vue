6eb19a5704ea5ab8fdafd4f3e9acecdb07277e82
NoCap940 <riplui666@gmail.com>
2024-03-02 19:21:22 -0600

    Se agrego el componente de prueba en App.vue

--- DevRutasGO/src/App.vue
+++ DevRutasGO/src/App.vue
@@ -1,20 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import TestComponent from './components/TestComponent.vue'
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <TestComponent></TestComponent>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
