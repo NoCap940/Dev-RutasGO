ef185a4b1b7d9b7c29d12d2a88c8551082b4cc98
NoCap940 <riplui666@gmail.com>
2024-03-02 19:39:28 -0600

    Se elimino el logo de Vue de la pagina principal de App

--- DevRutasGO/src/App.vue
+++ DevRutasGO/src/App.vue
@@ -5,7 +5,6 @@ import './assets/styles.css'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
     <TestComponent></TestComponent>
   </header>
 </template>
