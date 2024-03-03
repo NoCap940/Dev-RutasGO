d8ecc61a393e2520f6cfb62140e7b021534b0fdc
NoCap940 <riplui666@gmail.com>
2024-03-02 19:31:28 -0600

    Se elimino la etiqueta style de App.vue y se importo styles.css

--- DevRutasGO/src/App.vue
+++ DevRutasGO/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import TestComponent from './components/TestComponent.vue'
+import './assets/styles.css'
 </script>
 
 <template>
@@ -9,31 +10,3 @@ import TestComponent from './components/TestComponent.vue'
   </header>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
